2f9f292ac98c60351091c9ba6edb142691d9461e
Nathan Hull <NH-Research.nathan.sh.research@gmail.com>
2026-01-14 18:15:45 +0000

    Beginning of data pre-processing

--- Data loading and processing.py
+++ Data loading and processing.py
@@ -11,16 +11,31 @@ from sklearn.metrics import confusion_matrix
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 import kagglehub
+import os
+
+path = kagglehub.dataset_download("sonalshinde123/daily-water-intake-and-hydration-patterns-dataset")
+
+print("Path to dataset files:", path)
 
 
+directory_listing = os.listdir(path) # get a list of the directories in that path
 
-array = np.arange(1,22,3)
+print(directory_listing) # Print the directory list
 
-print(array)
+csv_directory = directory_listing[0] # grab the first index (the csv) as there is only one file - will use as data
 
+csv_path = os.path.join(path, f"{csv_directory}") # use the os module and use path to get the entries full path name and use join to 
 
-df = pd.DataFrame(array)
+print(csv_path)
 
-print(df)
+csv = pd.read_csv(csv_path)
 
+print(csv.shape)
+
+raw_target = csv.iloc[:,6]   #use iloc in pandas and straight x[] in numpy
+
+print(raw_target.iloc[0])
+
+# raw_input = csv.iloc[:,0:6]
